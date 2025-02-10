89b55cf1dd8ecf3e82f5e1908faef8d81343c363
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2025-02-10 02:20:41 -0300

    feat(fighters): adds fighters endpoints and model

---
+++ app/fighters/controller.py
@@ -0,0 +1,15 @@
+from flask import Blueprint
+
+from app.fighters.model import Fighter
+from app.utils import create_pagination, ApiPagination
+
+fighters_bp = Blueprint('fighters', __name__, url_prefix='/fighters')
+
+@fighters_bp.route('/')
+def index(page: int | None=None) -> dict[str, ApiPagination | list[Fighter]]:
+    return create_pagination(Fighter(), page)
+
+@fighters_bp.route('/<slug>')
+def detail(slug: str) -> Fighter:
+    fighter = Fighter.query.filter(Fighter.slug == slug).first()
+    return fighter.dto()
