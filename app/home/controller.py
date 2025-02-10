83b9aeb31f41731b9a5eb706dfea4a3e241cf83e
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2025-02-10 02:22:00 -0300

    feat(home): adds simple index for the API

---
+++ app/home/controller.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+
+home_bp = Blueprint('home', __name__, url_prefix='/')
+
+@home_bp.route('/')
+def home():
+    return (
+        '<h1 style="text-align: center;">The BJJ API 🌐 🔌</h1>'
+        '<ul><li></span><a href=\'/fighters\'>Fighters</a> 🥋</span></li></ul>'
+        '<ul><li></span><a href=\'/teams\'>Teams</a> 🤼</span></li></ul>'
+        '<ul><li></span><a href=\'/lineages\'>Lineages</a> 🌳</span></li></ul>'
+        '<ul><li></span><a href=\'/achievements\'>Achievements</a> 🏅</span></li></ul>'
+    )
