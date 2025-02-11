3308e67eaf988d76b8fb0e67d9415a031fed15b5
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2025-02-11 15:55:47 -0300

    feat(swagger): adds reference for yml files in achievements controller

--- app/achievements/controller.py
+++ app/achievements/controller.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flasgger import swag_from
+from flask import Blueprint, make_response, Response
+from werkzeug.exceptions import HTTPException
 
 from app.achievements.model import Achievement
 from app.utils import create_pagination
@@ -6,10 +8,19 @@ from app.utils import create_pagination
 achievements_bp = Blueprint('achievements', __name__, url_prefix='/achievements')
 
 @achievements_bp.route('/')
-def index(page: int | None=None) -> dict[str, int | bool | list[Achievement]]:
-    return create_pagination(Achievement(), page)
+@swag_from('../../docs/achievements.yml')
+def index(page: int | None=None) -> Response:
+    try:
+        response = create_pagination(Achievement(), page)
+        return make_response(response, 200)
+    except HTTPException:
+        return make_response('Not Found', 404)
 
-@achievements_bp.route('/<int:id>')
-def detail(achievement_id: int) -> Achievement:
-    achievement = Achievement.query.get(achievement_id)
-    return achievement.dto()
+@achievements_bp.route('/<int:achievement_id>')
+@swag_from('../../docs/achievement.yml')
+def detail(achievement_id: int) -> Response:
+    try:
+        achievement = Achievement.query.get(achievement_id)
+        return make_response(achievement.dto(), 200)
+    except AttributeError:
+        return make_response('Not Found', 404)
