a26d0cbe988053b06f364aea486ff4463b373a44
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2025-02-11 14:46:59 -0300

    feat(picoss): add picocss cdn for a better style in the api home page

--- app/home/controller.py
+++ app/home/controller.py
@@ -5,10 +5,15 @@ home_bp = Blueprint('home', __name__, url_prefix='/')
 @home_bp.route('/')
 def home():
     return (
+        '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">'
+        '<body>'
+        '<main class="container">'
         '<h1 style="text-align: center;">The BJJ API 🌐 🔌</h1>'
-        '<ul><li></span><a href=\'/fighters\'>Fighters</a> 🥋</span></li></ul>'
-        '<ul><li></span><a href=\'/teams\'>Teams</a> 🤼</span></li></ul>'
-        '<ul><li></span><a href=\'/lineages\'>Lineages</a> 🌳</span></li></ul>'
-        '<ul><li></span><a href=\'/achievements\'>Achievements</a> 🏅</span></li></ul>'
-        '<ul><li></span><a href=\'/docs\'>API Docs</a> 📄💻</span></li></ul>'
+        '<ul><li></span><a href="/fighters">Fighters</a> 🥋</span></li></ul>'
+        '<ul><li></span><a href="/teams">Teams</a> 🤼</span></li></ul>'
+        '<ul><li></span><a href="/lineages">Lineages</a> 🌳</span></li></ul>'
+        '<ul><li></span><a href="/achievements">Achievements</a> 🏅</span></li></ul>'
+        '<ul><li></span><a href="/docs">API Docs</a> 📄💻</span></li></ul>'
+        '</main>'
+        '</body>'
     )
