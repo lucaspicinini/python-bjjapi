86fe2a675bfead077c369b867d237cc1abf89eab
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2025-02-11 16:05:45 -0300

    feat(swagger): adds reference for yml files in lineages controller

--- app/lineages/controller.py
+++ app/lineages/controller.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flasgger import swag_from
+from flask import Blueprint, make_response, Response
+from werkzeug.exceptions import HTTPException
 
 from app.lineages.model import Lineage
 from app.utils import create_pagination
@@ -6,10 +8,19 @@ from app.utils import create_pagination
 lineages_bp = Blueprint('lineages', __name__, url_prefix='/lineages')
 
 @lineages_bp.route('/')
-def index(page: int | None=None) -> dict[str, int | bool | list[Lineage]]:
-    return create_pagination(Lineage(), page)
+@swag_from('../../docs/lineages.yml')
+def index(page: int | None=None) -> Response:
+    try:
+        response = create_pagination(Lineage(), page)
+        return make_response(response, 200)
+    except HTTPException:
+        return make_response('Not Found', 404)
 
-@lineages_bp.route('/<int:id>')
-def detail(lineage_id: int) -> Lineage:
-    lineage = Lineage.query.get(lineage_id)
-    return lineage.dto()
+@lineages_bp.route('/<int:lineage_id>')
+@swag_from('../../docs/lineage.yml')
+def detail(lineage_id: int) -> Response:
+    try:
+        lineage = Lineage.query.get(lineage_id)
+        return make_response(lineage.dto(), 200)
+    except AttributeError:
+        return make_response('Not Found', 404)
