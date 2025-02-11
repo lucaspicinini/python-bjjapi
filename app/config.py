c686b3d4bbd5176f5481fd0d9fbebb2fa1d001c4
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2025-02-10 22:54:48 -0300

    fix(database): deal with db closing by timeout

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_ENGINE_OPTIONS={'pool_pre_ping': True}
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevConfig(Config):
