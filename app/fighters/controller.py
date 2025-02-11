285ac8d6a648d54f2167e08b62ff03069d8ec318
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2025-02-11 14:11:07 -0300

    feat(swagger): add yml reference in fighters endpoints

--- app/fighters/controller.py
+++ app/fighters/controller.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flasgger import swag_from
+from flask import Blueprint, make_response, Response
+from werkzeug.exceptions import HTTPException
 
 from app.fighters.model import Fighter
 from app.utils import create_pagination
@@ -6,10 +8,19 @@ from app.utils import create_pagination
 fighters_bp = Blueprint('fighters', __name__, url_prefix='/fighters')
 
 @fighters_bp.route('/')
-def index(page: int | None=None) -> dict[str, int | bool | list[Fighter]]:
-    return create_pagination(Fighter(), page)
+@swag_from('../../docs/fighters.yml')
+def index(page: int | None=None) -> Response:
+    try:
+        response = create_pagination(Fighter(), page)
+        return make_response(response, 200)
+    except HTTPException:
+        return make_response('Not found', 404)
 
 @fighters_bp.route('/<slug>')
-def detail(slug: str) -> Fighter:
-    fighter = Fighter.query.filter(Fighter.slug == slug).first()
-    return fighter.dto()
+@swag_from('../../docs/fighter.yml')
+def detail(slug: str) -> Response:
+    try:
+        fighter = Fighter.query.filter(Fighter.slug == slug).first()
+        return make_response(fighter.dto(), 200)
+    except AttributeError:
+        return make_response('Not found', 404)
